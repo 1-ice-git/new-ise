55dbb9d55920ad5790a4987b9dd7fde94746ffc3
Mauro Arduini <mauro.arduini@ritspa.it>
2016-12-23 11:42:07 +0100

    Aggiunto fontawesome; modificata versione jquery da 3.1.1 a 2.2.4; Aggiunto il controllo data tables; Aggiunto il jqueryui 1.12.1 con tema;

--- NewISE/App_Start/BundleConfig.cs
+++ NewISE/App_Start/BundleConfig.cs
@@ -10,7 +10,36 @@ namespace NewISE.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/moment").Include(
+                        "~/Scripts/moment*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/globalize").Include(
+                        "~/Scripts/globalize.0.1.3/globalize.js*",
+                        "~/Scripts/globalize.0.1.3/cultures/globalize.culture.it-IT.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Content/jquery-ui-1.12.1.custom/jquery-ui.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/jqueryuiCSS").Include(
+                      "~/Content/jquery-ui-1.12.1.custom/jquery-ui.min.css",
+                      "~/Content/jquery-ui-1.12.1.custom/jquery-ui.structure.min.css",
+                      "~/Content/jquery-ui-1.12.1.custom/jquery-ui.theme.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/font-awesome.min.css"));
+
+
+
+
+
         }
     }
 }
