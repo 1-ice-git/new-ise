03a1b0d87bb3a15f2684add2c65df6ed3b1341e6
Mauro Arduini <yoravas@gmail.com>
2016-12-27 21:08:08 +0100

    Aggiunto riferimento per abilitare il GetOwinContext (Microsoft.Owin.Host.SystemWeb)

---
+++ NewISE/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Owin.Host.SystemWeb;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace NewISE.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IAuthenticationManager Authentication
+        {
+            get { return HttpContext.GetOwinContext().Authentication; }
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+
+            {
+                return Url.Action("Home", "Home");
+            }
+
+            return returnUrl;
+        }
+
+
+
+
+
+    }
+}
