112f8a732de9e27dbcc202570ffb06d83688a76a
Daniele Fantini <daniele.fantini@ritspa.it>
2017-01-31 14:27:34 +0100

    Data Annotations

--- NewISE/Models/RegoleCalcoloModel.cs
+++ NewISE/Models/RegoleCalcoloModel.cs
@@ -10,29 +10,32 @@ namespace NewISE.Models
     public class RegoleCalcoloModel
     {
         [Key]
-        public long idRegola { get; set; }
+
         [Required(ErrorMessage = "Id Regola richiesto")]
+        public long idRegola { get; set; }
+        
         public long idTipoRegolaCalcolo { get; set; }
         
         public long idNormaCalcolo { get; set; }
-        
-        public string formulaRegolaCalcolo { get; set; }
+
         [StringLength(20, ErrorMessage = "Il campo Formula Regola Calcolo deve contenere 1000 caratteri")]
-        public DateTime dataInizioValidita { get; set; }
+        public string formulaRegolaCalcolo { get; set; }
+
         // [Required(ErrorMessage = "Data Inizio Validita richiesta")]
         [Required]
         [Display(Name = "Data Inizio Validita ")]
         [DataType(DataType.Date)]
+        public DateTime dataInizioValidita { get; set; }
 
-        public DateTime dataFineValidita { get; set; }
         // [Required(ErrorMessage = "Data Fine Validita richiesta")]
         [Required]
         [Display(Name = "Data Fine Validita ")]
         [DataType(DataType.Date)]
+        public DateTime dataFineValidita { get; set; }
 
-        public long annullato { get; set; }
         [Range(1, 0)]
-
+        public long annullato { get; set; }
+        
         public TIPOREGOLACALCOLO tiporegolaCalcolo { get; set; }
         public REGOLACALCOLO_RIDUZIONI regolaCalcoloRiduzioni { get; set; }
 
