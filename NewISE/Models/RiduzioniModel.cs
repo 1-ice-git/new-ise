a6b842388b25acf3ec5f8c6c216e740100d10318
Daniele Fantini <daniele.fantini@ritspa.it>
2017-01-31 13:52:55 +0100

    Gestione Data Annotations

--- NewISE/Models/RiduzioniModel.cs
+++ NewISE/Models/RiduzioniModel.cs
@@ -10,22 +10,26 @@ namespace NewISE.Models
     public class RiduzioniModel
     {
         [Key]
-        public long idRiduzioni { get; set; }
         [Required]
         [Display(Name = "Id Riduzioni")]
-        public DateTime dataInizioValidita { get; set; }
+        public long idRiduzioni { get; set; }
+
         [Required]
         [Display(Name = "Data Inizio Validita ")]
         [DataType(DataType.Date)]
-        public DateTime dataFinevalidita { get; set; }
+        public DateTime dataInizioValidita { get; set; }
+
         [Required]
         [Display(Name = "Data Fine Validita ")]
         [DataType(DataType.Date)]
-        public long percentuale { get; set; }
-        [Range(11, 8)]
-        public long annullato { get; set; }
-        [Range(1, 0)]
+        public DateTime dataFinevalidita { get; set; }
 
+        [Range(11, 8)]
+        public long percentuale { get; set; }
+
+        [Range(1, 0)]
+        public long annullato { get; set; }
+        
         public REGOLACALCOLO_RIDUZIONI regolaCalcoloRiduzioni { get; set; }
 
     }
