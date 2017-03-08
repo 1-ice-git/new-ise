f4363d71f537f0a02686becdf6e51c8cb7aaa684
Daniele Fantini <daniele.fantini@ritspa.it>
2017-03-08 15:59:42 +0100

    Inserita classe invio email.

---
+++ NewISE/Interfacce/Modelli/Destinatario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace NewISE.Interfacce.Modelli
+{
+    public class Destinatario
+    {
+        public string Nominativo { get; set; }
+        public string EmailDestinatario { get; set; }
+    }
+}
---
+++ NewISE/Interfacce/Modelli/Mittente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewISE.Interfacce.Modelli
+{
+    public class Mittente
+    {
+        public string Nominativo { get; set; }
+        public string EmailMittente { get; set; }
+    }
+}
