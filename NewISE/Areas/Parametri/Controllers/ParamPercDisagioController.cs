e45cee80703e5818af772458b1c9351f0733bea8
Daniele Fantini <daniele.fantini@ritspa.it>
2017-03-27 17:56:50 +0200

    Lavorazione Tabelle Parametriche

--- NewISE/Areas/Parametri/Controllers/ParamPercDisagioController.cs
+++ NewISE/Areas/Parametri/Controllers/ParamPercDisagioController.cs
@@ -13,19 +13,136 @@ namespace NewISE.Areas.Parametri.Controllers
 
         [AcceptVerbs(HttpVerbs.Get | HttpVerbs.Post)]
         [Authorize(Roles = "1 ,2")]
-        public ActionResult PercentualeDisagio()
+        public ActionResult PercentualeDisagio(bool escludiAnnullati, decimal idUfficio = 0)
         {
             List<PercentualeDisagioModel> libm = new List<PercentualeDisagioModel>();
+            var r = new List<SelectListItem>();
+            List<UfficiModel> llm = new List<UfficiModel>();
+
             try
             {
+                using (dtUffici dtl = new dtUffici())
+                {
+                    llm = dtl.GetUffici().OrderBy(a => a.descrizioneUfficio).ToList();
 
+                    if (llm != null && llm.Count > 0)
+                    {
+                        r = (from t in llm
+                             select new SelectListItem()
+                             {
+                                 Text = t.descrizioneUfficio,
+                                 Value = t.idUfficio.ToString()
+                             }).ToList();
+
+                        if (idUfficio == 0)
+                        {
+                            r.First().Selected = true;
+                            idUfficio = Convert.ToDecimal(r.First().Value);
+                        }
+                        else
+                        {
+                            r.Where(a => a.Value == idUfficio.ToString()).First().Selected = true;
+                        }
+                    }
+
+                    ViewBag.LivelliList = r;
+                }
+
+                using (dtPercentualeDisagio dtib = new dtPercentualeDisagio())
+                {
+                    if (escludiAnnullati)
+                    {
+                        escludiAnnullati = false;
+                        libm = dtib.getListPercentualeDisagio(idUfficio, escludiAnnullati).OrderBy(a => a.idUfficio).ThenBy(a => a.dataInizioValidita).ThenBy(a => a.dataFineValidita).ToList();
+                    }
+                    else
+                    {
+                        libm = dtib.getListPercentualeDisagio(idUfficio).OrderBy(a => a.idUfficio).ThenBy(a => a.dataInizioValidita).ThenBy(a => a.dataFineValidita).ToList();
+                    }
+                }
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-
                 return PartialView("ErrorPartial");
             }
+
+            ViewBag.escludiAnnullati = escludiAnnullati;
+
             return PartialView(libm);
         }
+
+        [HttpPost]
+        [Authorize(Roles = "1 ,2")]
+        public ActionResult PercentualeDisagioLivello(decimal idUfficio, bool escludiAnnullati)
+        {
+            List<PercentualeDisagioModel> libm = new List<PercentualeDisagioModel>();
+            var r = new List<SelectListItem>();
+            List<UfficiModel> llm = new List<UfficiModel>();
+
+            try
+            {
+                using (dtUffici dtl = new dtUffici())
+                {
+                    llm = dtl.GetUffici().OrderBy(a => a.descrizioneUfficio).ToList();
+
+                    if (llm != null && llm.Count > 0)
+                    {
+                        r = (from t in llm
+                             select new SelectListItem()
+                             {
+                                 Text = t.descrizioneUfficio,
+                                 Value = t.idUfficio.ToString()
+                             }).ToList();
+                        r.Where(a => a.Value == idUfficio.ToString()).First().Selected = true;
+                    }
+
+                    ViewBag.LivelliList = r;
+                }
+
+                using (dtPercentualeDisagio dtib = new dtPercentualeDisagio())
+                {
+                    if (escludiAnnullati)
+                    {
+                        escludiAnnullati = false;
+                        libm = dtib.getListPercentualeDisagio(llm.Where(a => a.idUfficio == idUfficio).First().idUfficio, escludiAnnullati).OrderBy(a => a.idUfficio).ThenBy(a => a.dataInizioValidita).ThenBy(a => a.dataFineValidita).ToList();
+                    }
+                    else
+                    {
+                        libm = dtib.getListPercentualeDisagio(llm.Where(a => a.idUfficio == idUfficio).First().idUfficio).OrderBy(a => a.idUfficio).ThenBy(a => a.dataInizioValidita).ThenBy(a => a.dataFineValidita).ToList();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return PartialView("ErrorPartial");
+            }
+            ViewBag.escludiAnnullati = escludiAnnullati;
+
+            return PartialView("PercentualeDisagio", libm);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "1, 2")]
+        public ActionResult NuovaPercentualeDisagio(decimal idUfficio, bool escludiAnnullati)
+        {
+            var r = new List<SelectListItem>();
+            //IndennitaBaseModel ibm = new IndennitaBaseModel();
+
+            try
+            {
+                using (dtUffici dtl = new dtUffici())
+                {
+                    var lm = dtl.GetUffici(idUfficio);
+                    ViewBag.Livello = lm;
+                }
+                ViewBag.escludiAnnullati = escludiAnnullati;
+                return PartialView();
+            }
+            catch (Exception ex)
+            {
+                return PartialView("ErrorPartial");
+            }
+        }
+
     }
 }
