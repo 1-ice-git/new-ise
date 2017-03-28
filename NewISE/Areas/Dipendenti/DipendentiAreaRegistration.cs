26f7082a5407ee77132e4f9bebedaa3c55301299
Mauro Arduini <mauro.arduini@ritspa.it>
2017-03-28 16:36:42 +0200

    Lavorazione dipendenti

---
+++ NewISE/Areas/Dipendenti/DipendentiAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace NewISE.Areas.Dipendenti
+{
+    public class DipendentiAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Dipendenti";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Dipendenti_default",
+                "Dipendenti/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ NewISE/Areas/Dipendenti/Models/DipendentiModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewISE.Areas.Dipendenti.Models
+{
+    public class DipendentiModel
+    {
+        [Key]
+        public decimal idDipendente { get; set; }
+        [Required(ErrorMessage = "La matricola è richiesta.")]
+        [Display(Name = "Matricola")]
+        [Range(1, 999999, ErrorMessage = "Sono consentiti valori da 1 a 999999")]
+        public int matricola { get; set; }
+        [Required(ErrorMessage = "Il nome è richiesto.")]
+        [DataType(DataType.Text)]
+        [StringLength(30, ErrorMessage = "Il nome accetta un massimo di 30 caratteri.")]
+        [Display(Name = "Nome")]
+        public string nome { get; set; }
+        [Required(ErrorMessage = "Il cognome è richiesto.")]
+        [DataType(DataType.Text)]
+        [StringLength(30, ErrorMessage = "Il cognome accetta un massimo di 30 caratteri.")]
+        [Display(Name = "Cognome")]
+        public string cognome { get; set; }
+        [Required(ErrorMessage = "La data di assunzione è richiesta.")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data assunzione")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/mm/yyyy}")]
+        public DateTime dataAssunzione { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Data assunzione")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/mm/yyyy}")]
+        public DateTime? dataCessazione { get; set; }
+        [DataType(DataType.Text)]
+        [StringLength(100, ErrorMessage = "L'indirizzo accetta un massimo di 100 caratteri.")]
+        [Display(Name = "Indirizzo")]
+        public string indirizzo { get; set; }
+        [DataType(DataType.Text)]
+        [StringLength(10, ErrorMessage = "Il campo CAP accetta un massimo di 10 caratteri.")]
+        [Display(Name = "CAP")]
+        public string cap { get; set; }
+        [DataType(DataType.Text)]
+        [StringLength(30, ErrorMessage = "Il campo città accetta un massimo di 30 caratteri.")]
+        [Display(Name = "Città")]
+        public string citta { get; set; }
+        [DataType(DataType.Text)]
+        [StringLength(30, ErrorMessage = "Il campo provincia accetta un massimo di 30 caratteri.")]
+        [Display(Name = "Provincia")]
+        public string provincia { get; set; }
+        [EmailAddress(ErrorMessage = "L'e-mail non è valida.")]
+        [StringLength(50, ErrorMessage = "Il campo e-mail accetta un massimo di 50 caratteri.")]
+        [Display(Name = "E-mail")]
+        public string email { get; set; }
+        [DataType(DataType.Text)]
+        [StringLength(30, ErrorMessage = "Il campo telefono accetta un massimo di 30 caratteri.")]
+        [Display(Name = "Telefono")]
+        public string telefono { get; set; }
+        [DataType(DataType.Text)]
+        [StringLength(30, ErrorMessage = "Il campo fax accetta un massimo di 30 caratteri.")]
+        [Display(Name = "Fax")]
+        public string fax { get; set; }       
+        
+        [Display(Name = "Abilitato")]
+        public bool abilitato { get; set; }
+
+        
+        public string Nominativo
+        {
+            get
+            {
+                return cognome + " " + nome;
+            }
+        }
+
+        public IList<LivelliDipendentiModel> LivelloDipendenti { get; set; }
+
+    }
+}
