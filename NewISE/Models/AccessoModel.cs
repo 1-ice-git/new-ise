8765ee39260c5f7ad1cb9ae653b063bef4400c9c
Mauro Arduini <mauro.arduini@ritspa.it>
2017-03-28 14:23:36 +0200

    Funzione per registrare gli accessi terminata.

---
+++ NewISE/Models/AccessoModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewISE.Models
+{
+    public class AccessoModel
+    {
+        [Key]
+        public decimal idAccesso { get; set; }
+        [Required(ErrorMessage = "L'utente loggato = è richiesto.")]
+        public decimal idUtenteLoggato { get; set; }
+        [Required(ErrorMessage = "La data di accesso è richiesta.")]
+        [DataType(DataType.DateTime)]
+        public DateTime dataAccesso { get; set; }
+        [Required(ErrorMessage = "Il GUID è richiesto.")]        
+        public Guid guid { get; set; }
+
+        public UtenteAutorizzatoModel utenteAutorizzato { get; set; }
+    }
+}
---
+++ NewISE/Models/dtObj/objB/objAccesso.cs
@@ -0,0 +1,37 @@
+﻿using NewISE.Models.Tools;
+using System;
+
+namespace NewISE.Models.dtObj.objB
+{
+    public class objAccesso : IDisposable
+    {
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        public void Accesso(decimal idUtenteLoggato)
+        {
+            
+            using (dtAccesso dta = new dtAccesso())
+            {
+                try
+                {
+                    
+                    var am = new AccessoModel()
+                    {
+                        idUtenteLoggato = idUtenteLoggato,
+                        dataAccesso = DateTime.Now,
+                        guid = Guid.NewGuid()
+                    };
+
+                    dta.SetAccesso(am);
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+            }
+        }
+    }
+}
