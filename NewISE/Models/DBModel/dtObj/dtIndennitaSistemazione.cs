c089929d54266ad8961af53b757c0dc0468e0e3f
Mauro Arduini <mauro.arduini@ritspa.it>
2017-09-29 10:23:59 +0200

    Corretto css per dimensione div container e modificata logica attivazione dei pulsanti notifica richiesta e pratica conclusa per i titoli di viaggio. La modifica consiste nel poter inserire documenti dopo aver notificata la richiesta.

--- NewISE/Models/DBModel/dtObj/dtIndennitaSistemazione.cs
+++ NewISE/Models/DBModel/dtObj/dtIndennitaSistemazione.cs
@@ -5,11 +5,22 @@ using System.Web;
 
 namespace NewISE.Models.DBModel.dtObj
 {
+    public enum TipoTrasferimento
+    {
+        SedeEstero = 1,
+        EsteroEstero = 2,
+        EsteroEsteroStessaRegiona = 3
+    }
     public class dtIndennitaSistemazione : IDisposable
     {
         public void Dispose()
         {
             GC.SuppressFinalize(this);
+        }
+
+        public IndennitaSistemazioneModel GetIndennitaSistemazione()
+        {
+            return null;
         }
 
     }
