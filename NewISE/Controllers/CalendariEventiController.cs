89534e5a6b97492b97f58fa27cb3d0aaa4226616
Mauro Arduini <mauro.arduini@ritspa.it>
2017-10-27 14:34:52 +0200

    Revert "Variazione mag fam"
    This reverts commit cfeeccc7ad1d8efa123321177b601199cfbc0495.

---
+++ NewISE/Controllers/CalendariEventiController.cs
@@ -0,0 +1,32 @@
+﻿using NewISE.Models;
+using NewISE.Models.DBModel;
+using NewISE.Models.DBModel.dtObj;
+using NewISE.Models.dtObj;
+using NewISE.Models.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewISE.Models
+{
+    public class CalendariEventiController : Controller
+    {
+        // GET: CalendariEventi
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCalendarioEvento(DateTime? DataInizio)
+        {
+             DateTime? myDate;
+            if (DataInizio.HasValue)
+                myDate = DataInizio;
+            else myDate = null;
+            CalendarioEventiModel obj = new CalendarioEventiModel();
+            obj.DataInizioEvento = (DateTime)myDate;
+            return View(obj);
+        }
+    }
+}
---
+++ NewISE/Models/DBModel/dtObj/dtCalendarioEventi.cs
@@ -0,0 +1,68 @@
+﻿using NewISE.EF;
+using NewISE.Models.Tools;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewISE.Models.DBModel.dtObj
+{
+    public class dtCalendarioEventi : IDisposable
+    {
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        public void InsertCalendarioEvento(ref CalendarioEventiModel cem)
+        {
+            using (ModelDBISE db = new ModelDBISE())
+            {
+                CALENDARIOEVENTI ca = new CALENDARIOEVENTI();
+                ca.ANNULLATO = cem.Annullato;
+                ca.COMPLETATO = cem.Completato;
+                ca.DATACOMPLETATO = cem.DataCompletato;
+                ca.DATAINIZIOEVENTO = cem.DataInizioEvento;
+                ca.DATASCADENZA = cem.DataScadenza;
+                ca.IDFUNZIONIEVENTI = (decimal)cem.idFunzioneEventi;
+                ca.IDTRASFERIMENTO = cem.idTrasferimento;
+                db.CALENDARIOEVENTI.Add(ca);
+                int i = db.SaveChanges();
+                if (i <= 0)
+                {
+                    throw new Exception("Errore nella fase d'inserimento dell'evento per il calendario eventi.");
+                }
+                else
+                {
+                    cem.idCalendarioEventi = ca.IDCALENDARIOEVENTI; 
+
+                    Utility.SetLogAttivita(EnumAttivitaCrud.Inserimento, "Inserimento  dell'evento relativo al calendario eventi.",
+                        "CALENDARIOEVENTI", db, ca.IDTRASFERIMENTO, ca.IDCALENDARIOEVENTI);
+                }
+            }
+        }
+        public void ModificaInCompletatoCalendarioEvento(ref CalendarioEventiModel ca)
+        {
+            using (ModelDBISE db = new ModelDBISE())
+            {
+                decimal idCalev = ca.idCalendarioEventi;
+                var result = db.CALENDARIOEVENTI.SingleOrDefault(c => c.IDCALENDARIOEVENTI == idCalev);
+                if (result != null)
+                {
+                    result.COMPLETATO = true;
+                    int i = db.SaveChanges();
+                    if (i <= 0)
+                    {
+                        throw new Exception("Errore nella fase di modifica in 'Completato' dell'evento per il calendario eventi.");
+                    }
+                    else
+                    {
+                        Utility.SetLogAttivita(EnumAttivitaCrud.Modifica, "Modifica in 'Completato' dell'evento relativo al calendario eventi.",
+                          "CALENDARIOEVENTI", db, ca.idTrasferimento, ca.idCalendarioEventi);
+                    }
+                }               
+            }
+        }
+    }
+}
