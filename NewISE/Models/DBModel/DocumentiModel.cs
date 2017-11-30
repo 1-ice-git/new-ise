1c053fb8fb9df844231bb3a7740022ec26d4fd34
Mauro Arduini <mauro.arduini@ritspa.it>
2017-11-30 16:23:18 +0100

    Inserito nuovo campo "Modificato" su model DocumentiModel.

--- NewISE/Models/DBModel/DocumentiModel.cs
+++ NewISE/Models/DBModel/DocumentiModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -69,6 +70,10 @@ namespace NewISE.Models.DBModel
 
 
         public decimal fk_documenti { get; set; }
+
+        [DefaultValue(false)]
+        [Required]
+        public bool Modificato { get; set; }
 
 
         [Required(ErrorMessage = "Il Documento è richiesto.")]
