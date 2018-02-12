e32f9e394158c7945cf94558a76c9bdabf9257ab
Mauro Arduini <mauro.arduini@ritspa.it>
2018-02-12 12:22:50 +0100

    inserita funzione per autorizzare l'utente ice trasferito all'accesso di ise.

---
+++ NewISE/Models/dtObj/dtUtenteAutorizzato.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NewISE.EF;
+
+namespace NewISE.Models.dtObj
+{
+    public class dtUtenteAutorizzato : IDisposable
+    {
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+
+        public void SetAutorizzaUtenteTrasferito(decimal idDipendente, ModelDBISE db)
+        {
+            var dip = db.DIPENDENTI.Find(idDipendente);
+
+            if (dip.ABILITATO == false)
+            {
+                dip.ABILITATO = true;
+            }
+
+            //Effettuo una ricerca della matricola che voglio autorizzare per capire se già risulta autorizzata precedentemente.
+            var luamAttivoOld = dip.UTENTIAUTORIZZATI;
+
+            if (!luamAttivoOld?.Any() ?? false)
+            {
+                UTENTIAUTORIZZATI ua = new UTENTIAUTORIZZATI()
+                {
+                    IDRUOLOUTENTE = (decimal)EnumRuoloAccesso.Utente,
+                    IDDIPENDENTE = idDipendente,
+                    UTENTE = dip.MATRICOLA.ToString()
+                };
+
+                dip.UTENTIAUTORIZZATI.Add(ua);
+
+                int i = db.SaveChanges();
+
+                if (i <= 0)
+                {
+                    throw new Exception("Errore nella fase di autorizzazione dell'utente " + dip.COGNOME + " " + dip.NOME + " (" + dip.MATRICOLA + ")");
+                }
+
+
+
+            }
+        }
+
+
+
+
+    }
+}
