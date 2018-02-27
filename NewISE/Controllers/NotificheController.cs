f86592b1fc4f345cc16c9e70a6eca991ec5c4d14
Jean Dieudonne Ravelomanantsoa <jean.ravelomanantsoa@valueson.it>
2018-02-27 10:21:46 +0100

    Lavorazioni notifiche

--- NewISE/Controllers/NotificheController.cs
+++ NewISE/Controllers/NotificheController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using NewISE.Models.Config;
+using NewISE.Models.Config.s_admin;
+using NewISE.Models.DBModel;
+using NewISE.Models.DBModel.dtObj;
+using NewISE.Models.Tools;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +14,67 @@ namespace NewISE.Controllers
     public class NotificheController : Controller
     {
         // GET: Notifiche
+        bool admin = false;
         public ActionResult Index()
         {
+            try
+            {
+                admin = Utility.Amministratore();
+                ViewBag.Amministratore = admin;               
+            }
+            catch (Exception)
+            {
+                return View("Error");
+            }
             return View();
+        }
+        public ActionResult ListaNotifiche()
+        {
+            decimal idMittenteLogato =  Utility.UtenteAutorizzato().idDipendente;
+            List<NotificheModel> nm = new List<NotificheModel>();
+            using (dtNotifiche dn = new dtNotifiche())
+            {
+                nm = dn.GetNotifiche(idMittenteLogato).ToList();
+            }
+            
+            return PartialView(nm);
+        }
+        public ActionResult NuovaNotifica()
+        {
+            var r = new List<SelectListItem>();
+            using (Config cfg = new Config())
+            {
+                sAdmin sad = new sAdmin();
+                sUtenteNormale utentiNormali = new sUtenteNormale();
+                sad = cfg.SuperAmministratore();
+                if (sad.s_admin.Count > 0)
+                {
+                    r = (from t in sad.s_admin
+                         select new SelectListItem()
+                         {
+                             Text = t.email,
+                             Value = t.email
+                         }).ToList();
+                    //r.Where(a => a.Value == idTipoContributo.ToString()).First().Selected = true;
+                }
+            }
+            ViewBag.ListaDestinatari = r;
+            return PartialView();
+        }
+        public ActionResult VisualizzaCorpoMessaggio(decimal idNotifica)
+        {
+            decimal idMittenteLogato = Utility.UtenteAutorizzato().idDipendente;
+            List<NotificheModel> nm = new List<NotificheModel>();
+            NotificheModel elem = new NotificheModel();
+            using (dtNotifiche dn = new dtNotifiche())
+            {
+                nm = dn.GetNotifiche(idMittenteLogato).ToList();
+                if(nm.Count()>0)
+                {
+                    elem = nm.First();
+                }
+            }
+            return PartialView(elem);           
         }
     }
 }
