6ca0500c32e9fbeefd992bca287e4125e0801bdc
Mauro Arduini <mauro.arduini@ritspa.it>
2018-04-27 10:01:05 +0200

    Iniziata lavorazione Interfaccia elaborazioni

--- NewISE/Controllers/ElaborazioniController.cs
+++ NewISE/Controllers/ElaborazioniController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NewISE.Models.Tools;
 
 namespace NewISE.Controllers
 {
@@ -11,7 +12,29 @@ namespace NewISE.Controllers
         // GET: Elaborazioni
         public ActionResult Index()
         {
+            bool admin = false;
+
+            try
+            {
+                admin = Utility.Amministratore();
+
+                ViewBag.Amministratore = admin;
+            }
+            catch (Exception)
+            {
+
+                return View("Error");
+            }
+
+
             return View();
+        }
+
+        public ActionResult SelezionaAnno(int anno = 0)
+        {
+
+
+            return PartialView();
         }
     }
 }
