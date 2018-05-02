e694338e7305db4fc84cadc1dde9ae4fdc77a577
Daniele Fantini <daniele.fantini@ritspa.it>
2018-05-02 17:49:40 +0200

    Gestione Indennita (Layout)

---
+++ NewISE/Controllers/IndennitaController.cs
@@ -0,0 +1,129 @@
+﻿using NewISE.Models;
+using NewISE.Models.DBModel;
+using NewISE.Models.DBModel.dtObj;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewISE.Controllers
+{
+    public class IndennitaController : Controller
+    {
+        // GET: Indennita
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult GestioneIndennita(decimal idTrasferimento)
+        {
+            try
+            {
+                TrasferimentoModel tm = new TrasferimentoModel();
+                using (dtTrasferimento dtt = new dtTrasferimento())
+                {
+                    //var msgmail = dtt.GetMessaggioAnnullaTrasf(idTrasferimento);
+                    //var msg = msgmail.corpoMsg;
+                    //ViewBag.msgAnnulla = msg;
+
+                    //tm = dtt.GetTrasferimentoById(idTrasferimento);
+
+                    //var dataPartenza = tm.dataPartenza.ToShortDateString();
+                    //ViewData.Add("dataPartenza", dataPartenza);
+                    ViewData.Add("Trasferimento", tm);
+                }
+
+                ViewBag.idTrasferimento = idTrasferimento;
+
+                return PartialView("GestioneIndennita", tm);
+            }
+            catch (Exception ex)
+            {
+                return PartialView("ErrorPartial", new MsgErr() { msg = ex.Message });
+            }
+        }
+
+
+        //public ActionResult GestioneIndennita(decimal idTrasferimento)
+        //{
+        //    try
+        //    {
+        //        ViewData["idTrasferimento"] = idTrasferimento;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return PartialView("ErrorPartial", new MsgErr() { msg = ex.Message });
+        //    }
+
+        //    return PartialView("AttivitaIndennita");
+        //}
+
+        public ActionResult AttivitaIndennita(decimal idTrasferimento)
+        {
+            try
+            {
+                using (dtTrasferimento dtt = new dtTrasferimento())
+                {
+                    var tr = dtt.GetTrasferimentoById(idTrasferimento);
+                    using (dtDipendenti dtd = new dtDipendenti())
+                    {
+                        var d = dtd.GetDipendenteByIDTrasf(idTrasferimento);
+                        if (tr != null && tr.HasValue())
+                        {
+                            ViewBag.idTrasferimento = tr.idTrasferimento;
+                        }
+                        else
+                        {
+                            throw new Exception("Nessun trasferimento per la matricola (" + d.matricola + ")");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return PartialView("ErrorPartial", new MsgErr() { msg = ex.Message });
+
+            }
+
+
+
+            return PartialView();
+        }
+
+        public JsonResult VerificaIndennita(decimal idTrasferimento)
+        {
+            ViewData["idTrasferimento"] = idTrasferimento;
+
+            try
+            {
+                if (idTrasferimento <= 0)
+                {
+                    throw new Exception(" non valorizzato");
+                }
+                using (dtTrasferimento dtt = new dtTrasferimento())
+                {
+                    dipInfoTrasferimentoModel trm = dtt.GetInfoTrasferimento(idTrasferimento);
+                    if (trm != null && (trm.statoTrasferimento == EnumStatoTraferimento.Attivo ||
+                        trm.statoTrasferimento == EnumStatoTraferimento.Terminato))
+                    {
+                        ViewData["idTrasferimento"] = idTrasferimento;
+
+                        return Json(new { VerificaIndennita = 1 });
+                    }
+                    else
+                    {
+                        return Json(new { VerificaIndennita = 0 });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { err = ex.Message });
+            }
+        }
+
+    }
+}
