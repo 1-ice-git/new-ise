c3213e0230d43b7dd1e9f4252f667df9413ebf5a
Mauro Arduini <mauro.arduini@ritspa.it>
2018-05-16 16:16:09 +0200

    lavorazion elaborazioni

---
+++ NewISE/Models/DBModel/TipoVoceModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewISE.Models.DBModel
+{
+    public class TipoVoceModel
+    {
+        [Key]
+        public decimal idTipoVoce { get; set; }
+        [Required(ErrorMessage = "Il valore è richiesto")]
+        [StringLength(100, ErrorMessage = "Sono ammessi un massimo di 100 caratteri.")]
+        [Display(Name = "Descrizione")]
+        public string descrizione { get; set; }
+
+    }
+}
