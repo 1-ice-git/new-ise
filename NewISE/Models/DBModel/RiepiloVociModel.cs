c895157145c464301a8d15b2d1c07762f18a644b
Daniele Fantini <daniele.fantini@ritspa.it>
2018-06-21 17:13:59 +0200

    wrk metodo GetRiepilogoVoci (Funzione Riepilogo Voci)

--- NewISE/Models/DBModel/RiepiloVociModel.cs
+++ NewISE/Models/DBModel/RiepiloVociModel.cs
@@ -7,14 +7,16 @@ namespace NewISE.Models.DBModel
     public class RiepiloVociModel
     {
         [Key]
-        [Required(ErrorMessage = "La data di operazione è richiesta.")]
-        [DataType(DataType.Date)]
         
+        [DataType(DataType.Date)]
         [Display(Name = "Data Operazione")]
         public DateTime dataOperazione { get; set; }
 
         [Display(Name = "Importo")]
         public decimal importo { get; set; }
+
+        public TipoLiquidazioneModel TipoLiquidazione { get; set; }
+        public TipoVoceModel TipoVoce { get; set; }
 
     }
 }
--- NewISE/Models/DBModel/dtObj/dtRiepilogoVoci.cs
+++ NewISE/Models/DBModel/dtObj/dtRiepilogoVoci.cs
@@ -16,38 +16,64 @@ namespace NewISE.Models.DBModel.dtObj
         public IList<RiepiloVociModel> GetRiepilogoVoci(decimal idTrasferimento)
         {
             List<RiepiloVociModel> lrvm = new List<RiepiloVociModel>();
+            List<TipoLiquidazioneModel> tlm = new List<TipoLiquidazioneModel>();
+            List<TipoVoceModel> tvm = new List<TipoVoceModel>();
 
-            try
+            TIPOLIQUIDAZIONE TL = new TIPOLIQUIDAZIONE();
+            TIPOMOVIMENTO TM = new TIPOMOVIMENTO();
+            TIPOVOCE TV = new TIPOVOCE();
+            ELABINDSISTEMAZIONE elabIndSist = new ELABINDSISTEMAZIONE();
+
+
+            using (ModelDBISE db = new ModelDBISE())
             {
-                using (ModelDBISE db = new ModelDBISE())
+                db.Database.BeginTransaction();
+
+                try
                 {
-
-                    var ll =
-                        db.TEORICI.Where(
-                            a =>
-                                a.ANNULLATO == false && a.INSERIMENTOMANUALE == false &&
-                                a.INSERIMENTOMANUALE == false)
-                            .OrderBy(a => a.ELABINDSISTEMAZIONE.IDPRIMASISTEMAZIONE)
-                            .ToList();
-
-
-                    lrvm = (from e in ll
-                                    select new RiepiloVociModel()
-                                    {
-                                        
-                                        dataOperazione = e.DATAOPERAZIONE,
-                                        importo = e.IMPORTO
-                                        
-                                    }).ToList();
-                        
+                    var t = db.TRASFERIMENTO.Find(idTrasferimento);
                     
-                    return lrvm;
+                    if (t != null && t.IDTRASFERIMENTO > 0)
+                    {
+                        var ll =
+                            db.TEORICI.Where(
+                                a =>
+                                    a.ANNULLATO == false && a.INSERIMENTOMANUALE == false &&
+                                    a.INSERIMENTOMANUALE == false)
+                                .OrderBy(a => a.ELABINDSISTEMAZIONE.IDPRIMASISTEMAZIONE)
+                                .ToList();
+
+                        lrvm = (from e in ll
+                                select new RiepiloVociModel()
+                                {
+                                    dataOperazione = e.DATAOPERAZIONE,
+                                    importo = e.IMPORTO,
+                                    TipoLiquidazione = new TipoLiquidazioneModel()
+                                    {
+
+                                    },
+                                    TipoVoce = new TipoVoceModel()
+                                    {
+
+                                    }
+                                }).ToList();
+
+
+                    }
+
+                    db.Database.CurrentTransaction.Commit();
+
                 }
+                catch (Exception ex)
+                {
+                    db.Database.CurrentTransaction.Rollback();
+                    throw ex;
+                }
+
             }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+
+            return lrvm;
+
         }
     }
 }
