8e8e3c67e2f4299e225f55f0f521f5580ee5d422
Mauro Arduini <mauro.arduini@ritspa.it>
2018-08-24 12:00:06 +0200

    Inserito campo nascondi sulle tabelle PAGATOCONDIVISOMAB e CANONEMAB.

--- NewISE/EF/CANONEMAB.cs
+++ NewISE/EF/CANONEMAB.cs
@@ -31,6 +31,7 @@ namespace NewISE.EF
         public decimal IMPORTOCANONE { get; set; }
         public System.DateTime DATAAGGIORNAMENTO { get; set; }
         public Nullable<decimal> FK_IDCANONE { get; set; }
+        public bool NASCONDI { get; set; }
     
         public virtual ATTIVAZIONEMAB ATTIVAZIONEMAB { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
