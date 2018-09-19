9bc1077dfac16a3e4211d0a1448a80f309c2bfef
Daniele Fantini <daniele.fantini@ritspa.it>
2018-09-19 15:07:31 +0200

    Attività di sviluppo sulla funzione Statistiche

--- NewISE/Areas/Statistiche/Controllers/RiepiloghiIseMensileController.cs
+++ NewISE/Areas/Statistiche/Controllers/RiepiloghiIseMensileController.cs
@@ -2,7 +2,9 @@
 using NewISE.Areas.Statistiche.Models;
 using NewISE.Areas.Statistiche.Models.dtObj;
 using NewISE.Areas.Statistiche.RPTDataSet;
+using NewISE.EF;
 using NewISE.Models;
+using NewISE.Models.Enumeratori;
 using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Collections.Generic;
@@ -21,7 +23,72 @@ namespace NewISE.Areas.Statistiche.Controllers
         // GET: Statistiche/RiepiloghiIseMensile
         public ActionResult Index()
         {
-            return View();
+            
+            return PartialView();
         }
+
+        public ActionResult RptRiepiloghiIseMensile(string V_DATA1 = "", string V_DATA2 = "")
+        {
+            List<RptRiepiloghiIseMensileModel> rpt = new List<RptRiepiloghiIseMensileModel>();
+
+            try
+            {
+
+                using (ModelDBISE db = new ModelDBISE())
+                {
+                    var xx = db.ELABORAZIONI.ToList();
+
+                    List<decimal> lt2 = (from t in db.TEORICI
+                                         where t.ANNULLATO == false && t.INSERIMENTOMANUALE == false &&
+                                               //t.IDMESEANNOELAB == idAnnoMeseElaborato &&
+                                               t.ELABINDENNITA.Any(b => b.ANNULLATO == false) &&
+                                               t.DIRETTO == false &&
+                                               //t.VOCI.IDTIPOLIQUIDAZIONE == (decimal)EnumTipoLiquidazione.Contabilità &&
+                                               t.VOCI.IDVOCI == (decimal)EnumVociContabili.Ind_Sede_Estera
+                                         select t.IDTEORICI).ToList();
+
+
+                }
+
+
+                ReportViewer reportViewer = new ReportViewer();
+
+                reportViewer.ProcessingMode = ProcessingMode.Local;
+                reportViewer.SizeToReportContent = true;
+                reportViewer.Width = Unit.Percentage(100);
+                reportViewer.Height = Unit.Percentage(100);
+
+                var datasource = new ReportDataSource("DataSetRiepiloghiIseMensile");
+
+                reportViewer.Visible = true;
+                reportViewer.ProcessingMode = ProcessingMode.Local;
+                
+                reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"\Areas\Statistiche\RPT\RptRiepiloghiIseMensile.rdlc"; 
+                reportViewer.LocalReport.DataSources.Clear();
+
+                reportViewer.LocalReport.DataSources.Add(datasource);
+                reportViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSetRiepiloghiIseMensile", rpt));
+                reportViewer.LocalReport.Refresh();
+
+                
+                ReportParameter[] parameterValues = new ReportParameter[]
+                   {
+                        new ReportParameter ("Dal",V_DATA1),
+                        new ReportParameter ("Al",V_DATA2)
+                   };
+
+                reportViewer.LocalReport.SetParameters(parameterValues);
+                ViewBag.ReportViewer = reportViewer;
+
+
+            }
+            catch (Exception ex)
+            {
+                return PartialView("ErrorPartial", new MsgErr() { msg = ex.Message });
+            }
+
+            return PartialView("RptRiepiloghiIseMensile");
+        }
+
     }
 }
---
+++ NewISE/Areas/Statistiche/Models/RptRiepiloghiIseMensileModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewISE.Areas.Statistiche.Models
+{
+    public class RptRiepiloghiIseMensileModel
+    {
+        [Display(Name = "Matricola")]
+        public string matricola { get; set; }
+        [Display(Name = "Nominativo")]
+        public string nominativo { get; set; }
+        [Display(Name = "Qualifica")]
+        public string qualifica { get; set; }
+        [Display(Name = "Sede")]
+        public string sede { get; set; }
+        [Display(Name = "Valuta")]
+        public string valuta { get; set; }
+        [Display(Name = "Codice Tipo Movimento")]
+        public string codice_tipo_movimento { get; set; }
+        [Display(Name = "Tipo Movimento")]
+        public string tipo_movimento { get; set; }
+        [Display(Name = "Data Decorrenza")]
+        public string data_decorrenza { get; set; }
+        [Display(Name = "Data Lettera")]
+        public string data_lettera { get; set; }
+        [Display(Name = "Data Operazione")]
+        public string data_operazione { get; set; }
+        [Display(Name = "Indennità Personale")]
+        public string indennita_personale { get; set; }
+        [Display(Name = "Sist. /Rientro Lorda")]
+        public string anticipo { get; set; }
+
+    }
+}
