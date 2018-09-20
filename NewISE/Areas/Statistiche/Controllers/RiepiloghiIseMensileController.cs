00d98bf9596982863a63ba99ea404c70689adffe
Daniele Fantini <daniele.fantini@ritspa.it>
2018-09-20 17:57:33 +0200

    Inserimento del PeriodoMeseAnno nelle DropdDownList (Funzione Riepiloghi ISE Mensile)

--- NewISE/Areas/Statistiche/Controllers/RiepiloghiIseMensileController.cs
+++ NewISE/Areas/Statistiche/Controllers/RiepiloghiIseMensileController.cs
@@ -4,7 +4,9 @@ using NewISE.Areas.Statistiche.Models.dtObj;
 using NewISE.Areas.Statistiche.RPTDataSet;
 using NewISE.EF;
 using NewISE.Models;
+using NewISE.Models.DBModel;
 using NewISE.Models.DBModel.dtObj;
+using NewISE.Models.dtObj.ModelliCalcolo;
 using NewISE.Models.Enumeratori;
 using Oracle.ManagedDataAccess.Client;
 using System;
@@ -23,11 +25,110 @@ namespace NewISE.Areas.Statistiche.Controllers
     {
         // GET: Statistiche/RiepiloghiIseMensile
         public ActionResult Index()
-        {
-            
+        {   
             return PartialView();
         }
+        public JsonResult PrelevaMesiAnniElab(string search)
+        {
+            List<Select2Model> ls2 = new List<Select2Model>();
+            List<MeseAnnoElaborazioneModel> lmaem = new List<MeseAnnoElaborazioneModel>();
 
+            try
+            {
+                using (dtElaborazioni dte = new dtElaborazioni())
+                {
+
+
+                    lmaem = dte.PrelevaAnniMesiElaborati().ToList();
+
+                    foreach (var mae in lmaem)
+                    {
+                        Select2Model s2 = new Select2Model()
+                        {
+                            id = mae.idMeseAnnoElab.ToString(),
+                            text = CalcoloMeseAnnoElaborazione.NomeMese((EnumDescrizioneMesi)mae.mese) + "-" + mae.anno.ToString("D4"),
+                        };
+
+                        ls2.Add(s2);
+                    }
+
+
+                }
+
+                if (search != null && search != string.Empty)
+                {
+                    ls2 = ls2.Where(a => a.text.ToUpper().Contains(search.ToUpper())).ToList();
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new { results = new List<Select2Model>(), err = ex.Message });
+            }
+
+            return Json(new { results = ls2, err = "" });
+        }
+        public ActionResult SelezionaMeseAnno(int mese = 0, int anno = 0)
+        {
+            var rMeseAnno = new List<SelectListItem>();
+            List<MeseAnnoElaborazioneModel> lmaem = new List<MeseAnnoElaborazioneModel>();
+
+            try
+            {
+                if (anno == 0)
+                {
+                    anno = DateTime.Now.Year;
+                }
+
+                if (mese == 0)
+                {
+                    mese = DateTime.Now.Month;
+                }
+
+                using (dtElaborazioni dte = new dtElaborazioni())
+                {
+                    lmaem = dte.PrelevaAnniMesiElaborati().ToList();
+
+                    foreach (var item in lmaem)
+                    {
+
+                        rMeseAnno.Add(new SelectListItem()
+                        {
+                            Text = CalcoloMeseAnnoElaborazione.NomeMese((EnumDescrizioneMesi)item.mese) + "-" + item.anno.ToString("D4"),
+                            Value = item.idMeseAnnoElab.ToString()
+                        });
+
+                    }
+
+                    if (rMeseAnno.Exists(a => a.Text == CalcoloMeseAnnoElaborazione.NomeMese((EnumDescrizioneMesi)mese) + "-" + anno.ToString("D4")))
+                    {
+                        foreach (var item in rMeseAnno)
+                        {
+                            if (item.Text == CalcoloMeseAnnoElaborazione.NomeMese((EnumDescrizioneMesi)mese) + "-" + anno.ToString("D4"))
+                            {
+                                item.Selected = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        rMeseAnno.First().Selected = true;
+                    }
+
+                }
+
+                ViewData["ElencoMesiAnniElaborati"] = rMeseAnno;
+                ViewData["ElencoMesiAnniElaborati1"] = rMeseAnno;
+            }
+            catch (Exception ex)
+            {
+
+                return PartialView("ErrorPartial", new MsgErr() { msg = ex.Message });
+            }
+
+            return PartialView();
+        }
         public ActionResult RptRiepiloghiIseMensile(string dtIni, string dtFin)
         {
             List<RiepiloghiIseMensileModel> rim = new List<RiepiloghiIseMensileModel>();
