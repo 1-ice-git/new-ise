9ecb06a9e7f82be9182cb1e94eb4ff22ca6d34d9
Daniele Fantini <daniele.fantini@ritspa.it>
2018-09-27 15:08:32 +0200

    Report Storia del Dipendente

---
+++ NewISE/Areas/Statistiche/Controllers/StoriaDipendenteNewController.cs
@@ -0,0 +1,208 @@
+﻿
+using NewISE.Models;
+using NewISE.Models.DBModel;
+using NewISE.Models.dtObj;
+using NewISE.Models.DBModel.dtObj;
+using NewISE.Models.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Reporting.WebForms;
+using NewISE.EF;
+using NewISE.Areas.Statistiche.Models;
+using System.Web.UI.WebControls;
+
+namespace NewISE.Areas.Statistiche.Controllers
+{
+    public class StoriaDipendenteNewController : Controller
+    {
+        // GET: Statistiche/StoriaDipendenteNew
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+
+        public ActionResult DipendentiGepe(string matricola = "")
+        {
+            var rMatricola = new List<SelectListItem>();
+            var rNominativo = new List<SelectListItem>();
+            bool admin = false;
+            List<DipendentiModel> ldm = new List<DipendentiModel>();
+            DipendentiModel dm = new DipendentiModel();
+            AccountModel ac = new AccountModel();
+            try
+            {
+
+                admin = Utility.Amministratore(out ac);
+
+                using (dtDipendenti dtd = new dtDipendenti())
+                {
+                    if (!admin)
+                    {
+                        ldm.Add(dtd.GetDipendenteByMatricola(Convert.ToInt16(ac.utente)));
+                    }
+                    else if (matricola != string.Empty && admin == false)
+                    {
+                        ldm.Add(dtd.GetDipendenteByMatricola(Convert.ToInt16(matricola)));
+                    }
+                    else
+                    {
+                        ldm = dtd.GetDipendenti().ToList();
+                    }
+
+                    if (ldm.Count > 0)
+                    {
+                        foreach (var item in ldm)
+                        {
+                            rMatricola.Add(new SelectListItem()
+                            {
+                                Text = item.matricola.ToString(),
+                                Value = item.matricola.ToString()
+                            });
+
+                            rNominativo.Add(new SelectListItem()
+                            {
+                                Text = item.Nominativo + " (" + item.matricola.ToString() + ")",
+                                Value = item.matricola.ToString()
+                            });
+
+                        }
+
+                        rMatricola.Insert(0, new SelectListItem() { Text = "", Value = "" });
+
+                        rNominativo.Insert(0, new SelectListItem() { Text = "", Value = "" });
+
+                        if (matricola == string.Empty)
+                        {
+                            rMatricola.First().Selected = true;
+                            rNominativo.First().Selected = true;
+                        }
+                        else
+                        {
+                            foreach (var item in rMatricola)
+                            {
+                                if (matricola == item.Value)
+                                {
+                                    item.Selected = true;
+                                }
+                            }
+
+                            foreach (var item in rNominativo)
+                            {
+                                if (matricola == item.Value)
+                                {
+                                    item.Selected = true;
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+                if (matricola != string.Empty)
+                {
+                    dm = ldm.Where(a => a.matricola == Convert.ToInt16(matricola)).First();
+
+                    using (dtCDCGepe dtcdcg = new dtCDCGepe())
+                    {
+                        dm.cdcGepe = dtcdcg.GetCDCGepe(dm.idDipendente);
+                    }
+
+                    using (dtLivelliDipendente dtpl = new dtLivelliDipendente())
+                    {
+                        dm.livelloDipendenteValido = dtpl.GetLivelloDipendente(dm.idDipendente, DateTime.Now.Date);
+                    }
+                }
+
+
+                //ViewBag.ListDipendentiGepeMatricola = rMatricola.OrderBy(a=>a.Text);
+                ViewBag.ListDipendentiGepeNominativo = rNominativo.OrderBy(a => a.Text);
+                ViewBag.Amministratore = admin;
+                ViewBag.Matricola = dm.matricola;
+                ViewBag.Nominativo = dm.Nominativo;
+            }
+            catch (Exception ex)
+            {
+                return PartialView("ErrorPartial", new MsgErr() { msg = ex.Message });
+            }
+
+            return PartialView(dm);
+        }
+
+        public ActionResult RptStoriaDipendenteNew(decimal Nominativo)
+        {
+            List<StoriaDipendenteNewModel> rim = new List<StoriaDipendenteNewModel>();
+            List<RptStoriaDipendenteNewModel> rpt = new List<RptStoriaDipendenteNewModel>();
+
+            try
+            {
+
+                using (ModelDBISE db = new ModelDBISE())
+                {
+                    using (dtStoriaDipendenteNew dtStoriaDipendenteNew = new dtStoriaDipendenteNew())
+                    {
+                        rim = dtStoriaDipendenteNew.GetStoriaDipendenteNew(Nominativo, db).ToList();
+                    }
+
+                    if (rim?.Any() ?? false)
+                    {
+                        foreach (var lm in rim)
+                        {
+                            RptStoriaDipendenteNewModel rptds = new RptStoriaDipendenteNewModel()
+                            {
+                                matricola = lm.matricola,
+                                nome = lm.nome,
+                                cognome = lm.cognome
+
+
+                            };
+
+                            rpt.Add(rptds);
+                        }
+                    }
+                    
+                    ReportViewer reportViewer = new ReportViewer();
+
+                    reportViewer.ProcessingMode = ProcessingMode.Local;
+                    reportViewer.SizeToReportContent = true;
+                    reportViewer.Width = Unit.Percentage(100);
+                    reportViewer.Height = Unit.Percentage(100);
+
+                    var datasource = new ReportDataSource("DataSetStoriaDipendenteNew");
+
+                    reportViewer.Visible = true;
+                    reportViewer.ProcessingMode = ProcessingMode.Local;
+
+                    reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"\Areas\Statistiche\RPT\RptStoriaDipendenteNew.rdlc";
+                    reportViewer.LocalReport.DataSources.Clear();
+
+                    reportViewer.LocalReport.DataSources.Add(datasource);
+                    reportViewer.LocalReport.DataSources.Add(new ReportDataSource("DataSetStoriaDipendenteNew", rpt));
+                    reportViewer.LocalReport.Refresh();
+
+                    
+
+                    ReportParameter[] parameterValues = new ReportParameter[]
+                       {
+                        new ReportParameter ("Nominativo",Convert.ToString(Nominativo))
+                       };
+
+                    reportViewer.LocalReport.SetParameters(parameterValues);
+                    ViewBag.ReportViewer = reportViewer;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return PartialView("ErrorPartial", new MsgErr() { msg = ex.Message });
+            }
+
+            return PartialView("RptStoriaDipendenteNew");
+        }
+
+
+
+    }
+}
---
+++ NewISE/Models/DBModel/dtObj/dtStoriaDipendenteNew.cs
@@ -0,0 +1,53 @@
+﻿using NewISE.Areas.Statistiche.Models;
+using NewISE.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewISE.Models.DBModel.dtObj
+{
+    public class dtStoriaDipendenteNew : IDisposable
+    {
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        public IList<StoriaDipendenteNewModel> GetStoriaDipendenteNew(decimal Nominativo, ModelDBISE db)
+        {
+            List<StoriaDipendenteNewModel> rim = new List<StoriaDipendenteNewModel>();
+            
+            var lDipendenti =
+                   db.DIPENDENTI.Where(
+                       a =>
+                           a.MATRICOLA == Nominativo).ToList();
+
+
+
+            if (lDipendenti?.Any() ?? false)
+            {
+                foreach (var t in lDipendenti)
+                {
+
+                        StoriaDipendenteNewModel ldvm = new StoriaDipendenteNewModel()
+                        {
+                            matricola = t.MATRICOLA,
+                            nome = t.NOME,
+                            cognome = t.COGNOME
+                            
+                        };
+
+                        rim.Add(ldvm);
+                    
+                }
+            }
+
+            return rim;
+
+        }
+
+
+
+    }
+}
