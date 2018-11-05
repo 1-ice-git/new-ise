8ef6c8e89d994a247a793ea4b8141b597e1b7e90
Daniele Fantini <daniele.fantini@ritspa.it>
2018-11-05 11:44:59 +0100

    Test

--- NewISE/Controllers/IndennitaController.cs
+++ NewISE/Controllers/IndennitaController.cs
@@ -24,7 +24,7 @@ namespace NewISE.Controllers
     public class IndennitaController : Controller
     {
         // GET: Indennita
-        public ActionResult Index()
+        public ActionResult Index() 
         {
             return View();
         }        
