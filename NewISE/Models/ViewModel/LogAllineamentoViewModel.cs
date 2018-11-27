be5cf06d4989b02fea305acb283f2de93b57f04d
andrea gull <andreagull@ymail.com>
2018-11-27 09:59:08 +0100

    #36 rilascio issue 36

---
+++ NewISE/Models/ViewModel/LogAllineamentoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using NewISE.Models.DBModel;
+
+namespace NewISE.Models.ViewModel
+{
+    public class LogAllineamentoViewModel : LogAllineamentoModel
+    {       
+        [Required(ErrorMessage = "Data/Ora Inizio è richiesta.")]
+        [Display(Name = "Data/Ora Inizio")]
+        [DisplayFormat(DataFormatString = "{0:d t}")]
+        public DateTime? DataOraInizio { get; set; }
+    }
+}
