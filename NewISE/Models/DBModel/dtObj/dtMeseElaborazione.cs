c5dd24f363d4cb675b092e00a4132fff409c15d2
Mauro Arduini <mauro.arduini@ritspa.it>
2019-11-29 11:22:03 +0100

    Inibito l'inserimento del trasferimento con data inferiore alla data del mese di elaborazione.

---
+++ NewISE/Models/DBModel/dtObj/dtMeseElaborazione.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NewISE.EF;
+
+namespace NewISE.Models.DBModel.dtObj
+{
+    public class dtMeseElaborazione : IDisposable
+    {
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        public int MeseAnnoDaElaborare()
+        {
+            int annoMese = 0;
+
+            using (ModelDBISE db = new ModelDBISE())
+            {
+                MESEANNOELABORAZIONE meseAnnoElab = db.MESEANNOELABORAZIONE.OrderBy(a => a.ANNO).ThenBy(a => a.MESE).ToList().Last(a => a.CHIUSO == false);
+
+                if (meseAnnoElab.ANNO > 0)
+                {
+                    annoMese = Convert.ToInt32(meseAnnoElab.ANNO.ToString() + meseAnnoElab.MESE.ToString());
+                }
+            }
+
+            return annoMese;
+        }
+
+
+    }
+}
